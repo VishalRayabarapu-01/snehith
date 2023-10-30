a513246b7b483b22ac9334382b8281e39a6ed605
VishalRayabarapu-01 <vishalrayabarapu25@gmail.com>
2023-10-30 18:09:50 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "bootstrap/dist/css/bootstrap.css"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="text-center m-4">
+        <h1 style={{fontFamily : 'Playpen Sans'}}>Hi I'm Snehith🔥</h1>
+        <img src="/snehith.jpg" className="img-fluid rounded" />
+        <div className="m-4 p-3">
+         <a target="_blank" href="https://instagram.com/_snehith_nani_.02?igshid=NGVhN2U2NjQ0Yg==">Follow me on instagram</a> 
+         </div>
+      </div>
     </div>
   );
 }
